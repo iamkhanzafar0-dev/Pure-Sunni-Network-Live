6cf7d26572a58f72206efa5ee8f49a19498af4b5
iamkhanzafar0-dev <iamkhanzafar0@gmail.com>
2026-09-16 10:05:09 +0530

    Update build.gradle.kts for Android app configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,32 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.puresunninetwork.live"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.puresunninetwork.live"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
 dependencies {
 
     // Compose
