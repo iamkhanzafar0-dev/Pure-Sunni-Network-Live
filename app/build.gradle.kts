747f5b0118d1708170e0dfcd01892a0ebaf52f47
iamkhanzafar0-dev <iamkhanzafar0@gmail.com>
2026-09-16 09:27:56 +0530

    Update build.gradle.kts with new dependencies
    Added dependencies for Compose, Media3, and CameraX.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,8 @@ android {
 }
 
 dependencies {
+
+    // Compose
     val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
 
     implementation(composeBom)
@@ -35,8 +37,22 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Media3 / ExoPlayer
+    implementation("androidx.media3:media3-exoplayer:1.11.0")
+    implementation("androidx.media3:media3-ui:1.11.0")
+    implementation("androidx.media3:media3-session:1.11.0")
+
+    // CameraX
+    implementation("androidx.camera:camera-core:1.6.2")
+    implementation("androidx.camera:camera-camera2:1.6.2")
+    implementation("androidx.camera:camera-lifecycle:1.6.2")
+    implementation("androidx.camera:camera-view:1.6.2")
+    implementation("androidx.camera:camera-video:1.6.2")
+
 }
